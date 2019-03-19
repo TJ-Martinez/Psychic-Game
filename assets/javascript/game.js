3d87a021c611e78798f5fed05a174efcc349c4cd
CodeRed <antonio.martinez1653204@gmail.com>
2019-03-19 09:26:28 -0500

    After researching for hours I found .indexof

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,29 +2,41 @@ var letters= ['a', 'b', 'c', 'd', 'e', 'f' ,'g', 'h', 'i', 'j', 'k', 'l',
               'm','n', 'o', 'p', 'q', 'r', 's', 't', 'u','v', 'w', 'x', 
               'y', 'z'];
 
-var win= 0;
+var wins= 0;
 var losses= 0;
 var guesses= 9;                 // want to have an empty array for guessesSoFar so that when I create the .push function all the letters guessed will be pushed into this array and it will be visible to the user. 
-var left= 9;
 var guessesSoFar= [];
-var thewinningletter;
 
-//Want to create a function that has the computer create a new letter for the game. 
-    var newGame = function() {
-        thewinningletter = alphabet[Math.floor(Math.random() * alphabet.length)];
-    }
 
-document.onkeyup = function(event){
+document.onkeyup = function(event) {
+
     var userGuess = event.key;
-    left--; 
-    guessesSoFar.push(userGuess);
-    guessesLeft();
-    soFar();
-    if (left > 0){
-        if (userGuess == thewinningletter) {
-            wins++; 
-        }
-        
 
-    }
-}
+    var computerGuess = letters[Math.floor(Math.random() * letters.length)];
+
+    var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+    
+
+   if (options.indexOf(userGuess) > -1) {
+
+       if (userGuess === computerGuess) {
+           wins++;
+           guesses = 9;
+           guessesSoFar = [];
+       }
+
+       if (userGuess != computerGuess) {
+           guesses --;
+           guessesSoFar.push(userGuess);
+       }
+
+       if (guesses === 0) {
+
+       guesses = 9;
+       losses ++;
+       guessesSoFar = [];
+
+       
+   }
+
+   
