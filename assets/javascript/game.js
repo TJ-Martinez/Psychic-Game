179a1924f3ba46763fcc6586e3baea5d4a3a5b0f
CodeRed <antonio.martinez1653204@gmail.com>
2019-03-19 22:52:22 -0500

    added notes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,14 +14,17 @@ document.onkeyup = function(event) {
 
     var CPU = letters[Math.floor(Math.random() * letters.length)]; //computer will generate random letter from here using lettersArray
 
-    var TheRobotschoice = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+    var TheRobotschoice = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+                          "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
+                           "w", "x", "y", "z",];
 
     // I have decided to go with a conditional approach because it seems a lot clearer to me. Which is why I will be using var TheRobotschoice 
 // use .indexof because indexof allows me to associate each string with a number. This will help me with eliminating letters much easier.  
 // set the computerchoices.indexof(userGuess) to be greater than -1 so that there is no error. I did not know this until I started getting -1 error. 
 
-if (TheRobotschoice.indexOf(userGuess) > -1) { // so using .indexOf will take TheRobotschoiceArray and it'll correspond with the userGuess. So everytime the user keys in a letter then the "if's"
-                                               // will happen.                                                                                                                                                                                             
+if (TheRobotschoice.indexOf(userGuess) > -1) { // so using .indexOf will take TheRobotschoiceArray and 
+                                            //it'll correspond with the userGuess. 
+                                            //So everytime the user keys in a letter then the "if's will happen.                                                                                                                                                                                             
        if (userGuess === CPU) {
            wins++;
            guesses = 9;
