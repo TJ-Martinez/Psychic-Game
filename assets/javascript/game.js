1caf9dc1978504a0fcbccf745b8664d4bbd1ff3e
CodeRed <antonio.martinez1653204@gmail.com>
2019-03-19 22:25:23 -0500

    leaving notes on my code to help understand what i did

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,20 +14,26 @@ document.onkeyup = function(event) {
 
     var computerGuess = letters[Math.floor(Math.random() * letters.length)];
 
-    var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
-    
+    var TheRobotschoice = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
 
-   if (options.indexOf(userGuess) > -1) {
+    // I have decided to go with a conditional approach because it seems a lot clearer to me. Which is why I will be using var TheRobotschoice 
+// use .indexof because indexof allows me to associate each string with a number. This will help me with eliminating letters much easier.  
+// set the computerchoices.indexof(userGuess) to be greater than -1 so that there is no error. I did not know this until I started getting -1 error. 
+
+if (TheRobotschoice.indexOf(userGuess) > -1) {
 
        if (userGuess === computerGuess) {
            wins++;
            guesses = 9;
            guessesSoFar = [];
+           alert("Winner!")
        }
-
-       if (userGuess != computerGuess) {
+ 
+//Need to create a condition so that when a letter is guessed then it will be pushed to the the guessesSoFar and the # of guesses goes down. 
+       if (userGuess != computerGuess) { 
            guesses --;
            guessesSoFar.push(userGuess);
+
        }
 
        if (guesses === 0) {
@@ -35,8 +41,8 @@ document.onkeyup = function(event) {
        guesses = 9;
        losses ++;
        guessesSoFar = [];
+       alert("loser")
 
        
-   }
+   });
 
-   
